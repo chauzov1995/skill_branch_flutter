a4579ba0c46e6d369f15380ae3334758d3a63705
nikita <chauzov1995@yandex.ru>
2020-08-19 00:30:55 +0300

    a

---
+++ lib/screens/feed_screen.dart
@@ -0,0 +1,90 @@
+import 'package:FlutterGalleryApp/res/colors.dart';
+import 'package:FlutterGalleryApp/res/res.dart';
+import 'package:FlutterGalleryApp/widgets/widgets.dart';
+
+import 'package:flutter/material.dart';
+
+const String kFlutterDash =
+    'https://miro.medium.com/max/512/1*6Xz5i8qL9eu8RVISKIMZKQ.png';
+
+class Feed extends StatefulWidget {
+  Feed({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FeedsState();
+  }
+}
+
+class _FeedsState extends State<Feed> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: [
+                _buildItem(),
+                Divider(
+                  thickness: 2,
+                  color: AppColors.mercury,
+                ),
+              ],
+            );
+          }),
+    );
+  }
+
+  Widget _buildItem() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Photo(
+          photoLink: kFlutterDash,
+        ),
+        _buildPhotoMeta(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Text(
+            'This is Flutter Dash. I love him',
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+            style: AppStyles.h3.copyWith(color: AppColors.black),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildPhotoMeta() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              UserAvatar('https://skill-branch.ru/img/speakers/Adechenko.jpg'),
+              SizedBox(
+                width: 6,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Никита Чаузов', style: AppStyles.h2Black),
+                  Text(
+                    '@kaparray',
+                    style: AppStyles.h5Black.copyWith(color: AppColors.manatee),
+                  )
+                ],
+              )
+            ],
+          ),
+          LikeButton(10, true),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/photo_screen.dart
@@ -0,0 +1,141 @@
+import 'package:FlutterGalleryApp/res/colors.dart';
+import 'package:FlutterGalleryApp/res/styles.dart';
+import 'package:FlutterGalleryApp/widgets/widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const String kFLutterDash =
+    'https://miro.medium.com/max/512/1*6Xz5i8qL9eu8RVISKIMZKQ.png';
+
+class FullScreenImage extends StatefulWidget {
+  FullScreenImage({this.userName, this.altDescription, this.name, Key key})
+      : super(key: key);
+
+  final String userName;
+  final String altDescription;
+  final String name;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FullScreenImageState();
+  }
+}
+
+class _FullScreenImageState extends State<FullScreenImage> {
+  String userName;
+  String altDescription;
+  String name;
+
+  @override
+  void initState() {
+    super.initState();
+    userName = widget.userName;
+    altDescription = widget.altDescription;
+    name = widget.name;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo'),
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.back),
+          onPressed: () {},
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Photo(
+            photoLink: kFLutterDash,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: Text(
+              altDescription.toString(),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: AppStyles.h3.copyWith(color: AppColors.black),
+            ),
+          ),
+          _buildPhotoMeta(),
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LikeButton(2157, true),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () => true,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(7),
+                          child: Container(
+                            height: 36,
+                            width: 105,
+                            color: AppColors.dodgerBlue,
+                            child: Center(
+                              child: Text(
+                                'Save',
+                                style: AppStyles.h3,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      GestureDetector(
+                        onTap: () => true,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(7),
+                          child: Container(
+                            height: 36,
+                            width: 105,
+                            color: AppColors.dodgerBlue,
+                            child: Center(
+                              child: Text(
+                                'Visit',
+                                style: AppStyles.h3,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPhotoMeta() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          UserAvatar('https://skill-branch.ru/img/speakers/Adechenko.jpg'),
+          SizedBox(
+            width: 6,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(name.toString(), style: AppStyles.h1Black),
+              Text(
+                '@$userName',
+                style: AppStyles.h5Black.copyWith(color: AppColors.manatee),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
